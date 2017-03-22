b0c7d35646cfb862f624711e0af18c85a77220e4
MarionToussaint <marion.toussaint@telecomnancy.net>
2017-03-22 09:39:40 +0100

    exercice4 maj

--- app/src/main/java/com/example/mmtou/englishapplication/Exercise4Activity.java
+++ app/src/main/java/com/example/mmtou/englishapplication/Exercise4Activity.java
@@ -1,11 +1,16 @@
 package com.example.mmtou.englishapplication;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.example.mmtou.englishapplication.activity.Exercise3Activity;
@@ -20,6 +25,7 @@ public class Exercise4Activity extends AppCompatActivity{
     private StyleModel style = new StyleModel();
     private Exercise4Model model = new Exercise4Model();
     private ArrayList<CheckBox> listC= new ArrayList<CheckBox>();
+    public PopupWindow pw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,13 +50,40 @@ public class Exercise4Activity extends AppCompatActivity{
 
         style.changeFont("fonts/Aprikas_light_Demo.ttf", Exercise4Activity.this, listTextView);
 
-        System.out.println("WOO/"+((CheckBox)findViewById(R.id.verb)).isChecked());
-        System.out.println("OOO/"+((CheckBox)findViewById(R.id.verb)).isClickable());
         model.Clickable(((CheckBox)findViewById(R.id.verb)),((CheckBox)findViewById(R.id.verb1)),((CheckBox)findViewById(R.id.verb2)),((CheckBox)findViewById(R.id.verb3)));
         model.setLevel(model.listLevel1,listTextView);
-        model.check((Button)findViewById(R.id.button),listC);
+        //model.check((Button)findViewById(R.id.button),listC);
+
 
 
 
     }
+
+    public void showPopup(int[] answer, final String[] verb, int ans, int j){
+        ArrayList<TextView> l = model.addPop();
+        try {
+            LayoutInflater inflater = (LayoutInflater) Exercise4Activity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.pop4, (ViewGroup) findViewById(R.id.pop4));
+            pw = new PopupWindow(layout, 900, 500, true);
+            pw.showAtLocation(layout, Gravity.CENTER, 0, 0);
+            model.setChangePop(verb,answer,ans,j);
+            layout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    pw.dismiss();
+                    model.clear(listTextView,0,verb);
+                }
+            });
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    };
+
+    private View.OnClickListener cancel = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            pw.dismiss();
+        }
+    };
 }
--- app/src/main/java/com/example/mmtou/englishapplication/model/Exercise4Model.java
+++ app/src/main/java/com/example/mmtou/englishapplication/model/Exercise4Model.java
@@ -5,21 +5,27 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mmtou.englishapplication.Exercise4Activity;
 import com.example.mmtou.englishapplication.ExerciseChoiceActivity;
+import com.example.mmtou.englishapplication.R;
 import com.example.mmtou.englishapplication.activity.Exercise1ChoiceActivity;
 
 import org.w3c.dom.Text;
 
+import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
@@ -27,31 +33,33 @@ import java.util.StringTokenizer;
  * Created by mmtou on 08/03/2017.
  */
 public class Exercise4Model {
-    public String[] listLevel1 = new String[]{"read","feel", "play", "see",
+    public String[] listLevel1 = new String[]{"read", "feel", "play", "see",
             "listen", "do", "go", "make", "know", "help", "say", "think",
-            "like", "write", "forget", "eat","take", "bring", "cut", "clean",
+            "like", "write", "forget", "eat", "take", "bring", "cut", "clean",
             "watch", "be", "have", "meet", "put", "buy", "cook", "teach",
-            "catch", "find", "answer", "lose","want", "tell", "win", "sit",
+            "catch", "find", "answer", "lose", "want", "tell", "win", "sit",
             "sell", "build", "drink", "open"};
-    public String[] answerList = new String[]{"read","do","help","like","eat","clean","put","teach","want","sell"};
+    public int[] answerList = new int[]{1,2,3,0,1,2,2,3,0,0};
+    public ArrayList<TextView> pop = new ArrayList<>();
+    public Exercise4Activity ex = new Exercise4Activity();
 
-    public Exercise4Model(){
+    public Exercise4Model() {
     }
 
-    public void setLevel(String[] s,ArrayList<TextView> l){
-        int j=0;
-        for (int i=0; i<l.size()-2;i++){
+    public void setLevel(String[] s, ArrayList<TextView> l) {
+        int j = 0;
+        for (int i = 0; i < l.size() - 2; i++) {
             l.get(i).setText(s[j]);
             j++;
         }
     }
 
-    public void Clickable(final CheckBox c1,final CheckBox c2, final CheckBox c3, final CheckBox c4){
+    public void Clickable(final CheckBox c1, final CheckBox c2, final CheckBox c3, final CheckBox c4) {
         c1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView,
                                          boolean isChecked) {
-                if (isChecked){
+                if (isChecked) {
                     c2.setClickable(false);
                     c3.setClickable(false);
                     c4.setClickable(false);
@@ -62,7 +70,7 @@ public class Exercise4Model {
             @Override
             public void onCheckedChanged(CompoundButton buttonView,
                                          boolean isChecked) {
-                if (isChecked){
+                if (isChecked) {
                     c1.setClickable(false);
                     c3.setClickable(false);
                     c4.setClickable(false);
@@ -73,7 +81,7 @@ public class Exercise4Model {
             @Override
             public void onCheckedChanged(CompoundButton buttonView,
                                          boolean isChecked) {
-                if (isChecked){
+                if (isChecked) {
                     c1.setClickable(false);
                     c2.setClickable(false);
                     c4.setClickable(false);
@@ -84,7 +92,7 @@ public class Exercise4Model {
             @Override
             public void onCheckedChanged(CompoundButton buttonView,
                                          boolean isChecked) {
-                if (isChecked){
+                if (isChecked) {
                     c1.setClickable(false);
                     c2.setClickable(false);
                     c3.setClickable(false);
@@ -93,61 +101,81 @@ public class Exercise4Model {
         });
     }
 
-    public String answerChecked(CheckBox box1, CheckBox box2, CheckBox box3,CheckBox box4){
-        String answer = "";
+    public int answerChecked(CheckBox box1, CheckBox box2, CheckBox box3, CheckBox box4) {
+        int answer = 0;
         if ((!box1.isChecked()) && (!box2.isChecked()) && (!box3.isChecked()) && (!box4.isChecked()))
-            answer = "No Answer";
+            answer = 0;
         else if (box1.isChecked() && (!box2.isChecked()) && (!box3.isChecked()) && (!box4.isChecked()))
-            answer = (String) box1.getText();
+            answer = 1;
         else if (box2.isChecked() && (!box1.isChecked()) && (!box3.isChecked()) && (!box4.isChecked()))
-            answer = (String) box2.getText();
+            answer = 2;
         else if (box3.isChecked() && (!box2.isChecked()) && (!box1.isChecked()) && (!box4.isChecked()))
-            answer = (String) box3.getText();
+            answer = 3;
         else if ((!box1.isChecked()) && (!box2.isChecked()) && (!box3.isChecked()) && (box4.isChecked()))
-            answer = (String) box4.getText();
+            answer = 4;
 
         return answer;
     }
 
-    public void check(Button b,final ArrayList<CheckBox> l){
+    /*public void check(Button b, final ArrayList<CheckBox> l,int j) {
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String answer = answerChecked(l.get(0),l.get(1),l.get(2),l.get(3));
-                System.out.println("Answer is : "+answer);
+                int answer = answerChecked(l.get(0), l.get(1), l.get(2), l.get(3));
+                System.out.println("Answer is : " + answer);
+                ex.showPopup(answerList,listLevel1,answer,j);
+                j++;
             }
         });
-    }
-
-
-
-        /*if (i < (list.length - 1)) {
-            if (((list[i][1]).equals(p1.getText().toString())) && ((list[i][2]).equals(pp1.getText().toString()))) {
-                Toast.makeText(getApplicationContext(), "Correct Answer", Toast.LENGTH_SHORT).show();
-                point ++;
-            } else {
-                Toast.makeText(getApplicationContext(), "Wrong Answer", Toast.LENGTH_SHORT).show();
-                correction = ((list[i][0]) + "  |  " + (list[i][1]) + "  |  " + (list[i][2]) + "  |  " + (list[i][3]));
-                showPopup();
-            }
-            p1.getText().clear();
-            pp1.getText().clear();
-            verb.setText(list[i+1][0]);
-            page.setText((i+2) + " / " + ListVerb.listLevel1.length);
-        } else if (i == (list.length - 1)) {
-            if (((list[i][1]).equals(p1.getText().toString())) && ((list[i][2]).equals(pp1.getText().toString()))) {
-                Toast.makeText(getApplicationContext(), "Correct Answer", Toast.LENGTH_SHORT).show();
-                point++;
-            } else {
-                Toast.makeText(getApplicationContext(), "Wrong Answer", Toast.LENGTH_SHORT).show();
-                correction = ((list[i][0]) + "  |  " + (list[i][1]) + "  |  " + (list[i][2]) + "  |  " + (list[i][3]));
-                showPopup();
-            }
-        } else {
-            startActivity(new Intent(Level1EasyActivity.this, EasyActivity.class));
-        }
     }*/
 
+    public ArrayList<TextView> addPop(){
+        pop.add(((TextView) ex.pw.getContentView().findViewById(R.id.verb)));
+        pop.add(((TextView) ex.pw.getContentView().findViewById(R.id.verb1)));
+        pop.add(((TextView) ex.pw.getContentView().findViewById(R.id.verb2)));
+        pop.add(((TextView) ex.pw.getContentView().findViewById(R.id.verb3)));
+        pop.add(((TextView) ex.pw.getContentView().findViewById(R.id.verb4)));
+        pop.add(((TextView) ex.pw.getContentView().findViewById(R.id.verb5)));
+        pop.add(((TextView) ex.pw.getContentView().findViewById(R.id.verb6)));
+        pop.add(((TextView) ex.pw.getContentView().findViewById(R.id.verb7)));
+        pop.add(((TextView) ex.pw.getContentView().findViewById(R.id.textView3)));
+        pop.add(((TextView) ex.pw.getContentView().findViewById(R.id.textView2)));
+        return pop;
+    }
+
+    public void color(ArrayList<TextView> l, int answer, int[] answerList, int i){
+        l.get(answer-1).setTextColor(0xFFE5FFFA);
+        l.get(answerList[i]-1).setTextColor(0xFFE5FFFA);
+    }
+
+    public void clear(ArrayList<TextView> t, int index, String[] verb){
+        for (int i=0;i<4;i++){
+            t.get(i).setText(verb[index]);
+            index++;
+        }
+    }
+
+    public void setChangePop(String[] verb, int[] answer, int ans,int index){
+        ArrayList<TextView> l =addPop();
+        color(l,ans,answer,index);
+        int indexVerb = 4*index;
+
+        if (!(answer[index]==ans)) {
+            for (int k = 0; k < 4; k++) {
+                l.get(k).setText(verb[indexVerb]);
+                l.get(k + 4).setText(verb[indexVerb]);
+                indexVerb++;
+            }
+        }
+        else {
+            for (int k = 0; k < 4; k++) {
+                l.get(k).setText(verb[indexVerb]);
+                l.get(k + 4).setText("");
+            }
+            l.get(8).setText("Good Answer !");
+            l.get(9).setText("");
+        }
+    }
 
 
 }
